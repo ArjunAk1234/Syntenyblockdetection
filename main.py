ea21ab393e6a1de0977570394aa2e44100520636
ArjunAk1234 <144267463+ArjunAk1234@users.noreply.github.com>
2025-06-30 01:00:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -504,3 +504,9 @@ def internal_error(e):
         "status": "error"
     }), 500
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')  # Allow all
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    return response
